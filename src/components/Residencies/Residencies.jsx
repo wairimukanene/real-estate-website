cd0c663fb84f7c024fd5a93948d543339f5f3bb3
mwairimu <mwairimu@fintech-group.com>
2023-10-13 11:28:46 +0300

    added and styled the heading in the residencies component

---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
+import "swiper/css"
+import './Residencies.css'
+import data from '../../utils/slider.json'
+
+
+
+const Residencies = () => {
+  return (
+    <section className="r-wrapper">
+        <div className="paddings innerWidth r-container">
+            <div className="r-head flexColStart">
+                <span className='orangeText'>Best Choices</span><span className='primaryText'>Popular Residencies</span>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Residencies
