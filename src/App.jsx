27d042d077275ac0ca851aed0af980624d717a52
mwairimu <mwairimu@fintech-group.com>
2023-10-17 04:17:44 -0600

    fixing responsiveness on the footer section

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,8 @@ function App() {
       <Residencies/>
       <Value/>
       <Contact/>
-      {/* <GetStarted/> */}
-      {/* <Footer/> */}
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
